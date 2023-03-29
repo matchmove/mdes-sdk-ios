566378e6c3a7047ea5d6918acf1ba2b3cbe575d2
Vid <Vidhyadharan.mohanram@matchmove.com>
2023-03-29 12:36:20 +0530

    Inital Mdes commit.

---
+++ Mdes.xcframework/ios-arm64/Mdes.framework/Headers/Mdes.h
@@ -0,0 +1,18 @@
+//
+//  Mdes.h
+//  Mdes
+//
+//  Created by mm-Vidhyadharan on 21/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mdes.
+FOUNDATION_EXPORT double MdesVersionNumber;
+
+//! Project version string for Mdes.
+FOUNDATION_EXPORT const unsigned char MdesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mdes/PublicHeader.h>
+
+
